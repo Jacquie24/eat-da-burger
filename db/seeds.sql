46337c43b9891db037f56e70030c8fbfdcbfadee
Jacquie24 <jgcaption@yahoo.com>
2021-01-25 20:21:28 -0500

    Added schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burger (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burger (burger_name) VALUES ('Mushroom Swiss Burger');
+INSERT INTO burger (burger_name) VALUES ('BBQ Bacon Ranch Burger');
+INSERT INTO burger (burger_name) VALUES ('Old Fashioned with Cheese');
+INSERT INTO burger (burger_name) VALUES ('Black and Bleu Burger');
+INSERT INTO burger (burger_name) VALUES ('Impossible Burger');
